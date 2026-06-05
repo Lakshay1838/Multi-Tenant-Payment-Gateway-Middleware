6a56fa3d5872d81616f6c718f55ab8a419395b5e
lakshaypahujaepam <lakshay_pahuja@epam.com>
2026-06-05 22:17:37 +0530

    removed temporary CommandLineRunner for Verification

--- src/main/java/com/paymentgateway/PaymentGatewayApplication.java
+++ src/main/java/com/paymentgateway/PaymentGatewayApplication.java
@@ -1,26 +1,12 @@
 package com.paymentgateway;
 
-import com.paymentgateway.repository.IdempotencyRecordRepository;
-import com.paymentgateway.repository.TransactionRecordRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PaymentGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentGatewayApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner verifyRepositoryCounts(
-            TransactionRecordRepository transactionRecordRepository,
-            IdempotencyRecordRepository idempotencyRecordRepository) {
-        return args -> {
-            System.out.println("Transaction count: " + transactionRecordRepository.count());
-            System.out.println("Idempotency count: " + idempotencyRecordRepository.count());
-        };
     }
 }
